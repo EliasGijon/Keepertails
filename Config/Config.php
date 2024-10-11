83473467fadfe5760f4ab1ec7f4e3bffcc7e2eb8
Elias Gijon <zc.elias.gijon@gmail.com>
2024-10-11 17:43:17 +0000

    Lamp Server Config

--- Config/Config.php
+++ Config/Config.php
@@ -1,15 +1,15 @@
 <?php 
-	const BASE_URL = "http://localhost/tienda_virtual";
+	const BASE_URL = "http://kys.run.place";
 	//const BASE_URL = "https://abelosh.com/tiendavirtual";
 
 	//Zona horaria
 	date_default_timezone_set('America/Guatemala');
 
 	//Datos de conexión a Base de Datos
-	const DB_HOST = "localhost";
+	const DB_HOST = "lamp-mysql8";
 	const DB_NAME = "db_tiendavirtual";
 	const DB_USER = "root";
-	const DB_PASSWORD = "";
+	const DB_PASSWORD = "SysAdm24";
 	const DB_CHARSET = "utf8";
 
 	//Para envío de correo
